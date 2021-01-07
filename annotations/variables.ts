0a84ce09fc26b2dbc19841d627c9f101f16a7af5
estein1988 <estein1988@gmail.com>
2021-01-06 17:27:56 -0700

    Add annotations examples.

---
+++ annotations/variables.ts
@@ -0,0 +1,14 @@
+// number === type annotation
+// const apples: number = 5;
+let apples: number = 5;
+
+//apples = 'test test test'
+apples = 6
+
+let speed: string = 'fast';
+let hasName: boolean = true;
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+//built in objects
+let now: Date = new Date();
